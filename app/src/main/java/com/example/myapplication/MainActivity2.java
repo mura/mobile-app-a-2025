38ddd4594ca7e70d25d0073132328bcfb20a01b3
Yohei Murayama <muracchi@gmail.com>
2025-09-03 16:21:00 +0900

    画面遷移の演習

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -12,9 +14,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.myapplication.databinding.ActivityMain2Binding;
 
+import java.util.Optional;
+
 public class MainActivity2 extends AppCompatActivity {
 
     private ActivityMain2Binding binding;
+
+    private final ActivityResultLauncher<Intent> getActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                switch (result.getResultCode()) {
+                    case RESULT_OK -> {
+                        Optional.ofNullable(result.getData())
+                                .map(data -> data.getStringExtra("ret"))
+                                .map(text -> "Result: " + text)
+                                .ifPresent(text -> binding.resultText.setText(text));
+                    }
+                    case RESULT_CANCELED -> {
+                        binding.resultText.setText("Result: Canceled");
+                    }
+                    default -> {
+                        binding.resultText.setText("Result: Unknown(" + result.getResultCode() + ")");
+                    }
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +61,18 @@ public class MainActivity2 extends AppCompatActivity {
             var intent = new Intent(Intent.ACTION_VIEW);
             intent.setData(Uri.parse("https://www.yahoo.co.jp/"));
             startActivity(intent);
+        });
+
+        binding.sendButton.setOnClickListener(view -> {
+            var text = binding.editTextText2.getText().toString();
+            var intent = new Intent(this, MainActivity4.class);
+            intent.putExtra("text", text);
+            startActivity(intent);
+        });
+
+        binding.launchButton.setOnClickListener(view -> {
+            var intent = new Intent(this, MainActivity4.class);
+            getActivityResult.launch(intent);
         });
     }
 }
